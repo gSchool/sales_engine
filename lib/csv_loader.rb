b7410967d1a8f23e995d60cf4307881524cef0dc
lalalainexd <lalalainexd@gmail.com>
2013-02-12 20:42:09 -0700

    added tests for loading csv files

--- lib/csv_loader.rb
+++ lib/csv_loader.rb
@@ -1,19 +1,61 @@
 require 'csv'
 require 'merchant'
+require 'transaction'
+require 'invoice'
 require 'invoice_item'
+require 'item'
+require 'customer'
 
 
 class CsvLoader
 
   def self.load_merchants
-    contents = CSV.open('./data/merchants.csv', headers: true, header_converters: :symbol)
+    contents = CSV.open('./data/merchants.csv',
+                        headers: true,
+                        header_converters: :symbol)
     merchants = contents.collect { |row| Merchant.new(row.to_hash) }
     Merchant.add merchants
   end
 
+
   def self.load_invoice_items
-    contents = CSV.open('./data/merchants.csv', headers: true, header_converters: :symbol)
+    contents = CSV.open('./data/invoice_items.csv',
+                        headers: true,
+                        header_converters: :symbol)
     invoice_items = contents.collect { |row| InvoiceItem.new(row.to_hash) }
-    InvoiceItem.add(invoice_items)
+    InvoiceItem.add invoice_items
   end
+
+  def self.load_items
+    contents = CSV.open('./data/items.csv',
+                        headers: true,
+                        header_converters: :symbol)
+    items = contents.collect { |row| Item.new(row.to_hash) }
+    Item.add items
+  end
+
+  def self.load_transactions
+    contents = CSV.open('./data/transactions.csv',
+                        headers: true,
+                        header_converters: :symbol)
+    transactions = contents.collect { |row| Transaction.new(row.to_hash) }
+    Transaction.add transactions
+  end
+
+  def self.load_invoices
+    contents = CSV.open('./data/invoices.csv',
+                        headers: true,
+                        header_converters: :symbol)
+    invoice = contents.collect { |row| Invoice.new(row.to_hash) }
+    Invoice.add invoice
+  end
+
+  def self.load_customers
+    contents = CSV.open('./data/customers.csv',
+                        headers: true,
+                        header_converters: :symbol)
+    customers = contents.collect { |row| Customer.new(row.to_hash) }
+    Customer.add customers
+  end
+
 end
--- test/csv_loader_test.rb
+++ test/csv_loader_test.rb
@@ -3,6 +3,11 @@ require 'minitest/pride'
 
 require './lib/csv_loader'
 require './lib/merchant'
+require './lib/transaction'
+require './lib/invoice'
+require './lib/invoice_item'
+require './lib/item'
+require './lib/customer'
 
 class CsvLoaderTest < MiniTest::Unit::TestCase
 
@@ -13,10 +18,86 @@ class CsvLoaderTest < MiniTest::Unit::TestCase
     assert_kind_of Merchant, merchants.first
   end
 
-#should we move this to merchant_test?
-#testing implementation vs. output?
+
+  #should we move this to merchant_test?
+  #testing implementation vs. output?
   def test_it_stores_merchants
     merchants = CsvLoader.load_merchants
     assert_equal 100, Merchant.size
+  end
+
+  def test_it_loads_invoices
+    invoices = CsvLoader.load_invoices
+    assert_equal 4843, invoices.size
+
+    assert_kind_of Invoice, invoices.first
+  end
+
+
+  #should we move this to merchant_test?
+  #testing implementation vs. output?
+  def test_it_stores_invoices
+    invoices = CsvLoader.load_invoices
+    assert_equal 4843, Invoice.size
+  end
+
+  def test_it_loads_invoice_items
+    invoice_items = CsvLoader.load_invoice_items
+    assert_equal 21687, invoice_items.size
+
+    assert_kind_of InvoiceItem, invoice_items.first
+  end
+
+
+  #should we move this to merchant_test?
+  #testing implementation vs. output?
+  def test_it_stores_invoice_items
+    invoice_items = CsvLoader.load_invoice_items
+    assert_equal 21687, InvoiceItem.size
+  end
+
+  def test_it_loads_items
+    items = CsvLoader.load_items
+    assert_equal 2483, items.size
+
+    assert_kind_of Item, items.first
+  end
+
+
+  #should we move this to merchant_test?
+  #testing implementation vs. output?
+  def test_it_stores_items
+    items = CsvLoader.load_items
+    assert_equal 2483, Item.size
+  end
+
+  def test_it_loads_transactions
+    transactions = CsvLoader.load_transactions
+    assert_equal 5595, transactions.size
+
+    assert_kind_of Transaction, transactions.first
+  end
+
+
+  #should we move this to merchant_test?
+  #testing implementation vs. output?
+  def test_it_stores_transactions
+    transactions = CsvLoader.load_transactions
+    assert_equal 5595, Transaction.size
+  end
+
+  def test_it_loads_customers
+    customers = CsvLoader.load_customers
+    assert_equal 1000, customers.size
+
+    assert_kind_of Customer, customers.first
+  end
+
+
+  #should we move this to merchant_test?
+  #testing implementation vs. output?
+  def test_it_stores_customers
+    customers = CsvLoader.load_customers
+    assert_equal 1000, Customer.size
   end
 end
