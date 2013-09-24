e4c425f912e90e0e602ad2969b28c88e1a33b726
Simon Taranto <simon.taranto@gmail.com>
2013-09-24 15:05:27 -0600

    update customer repo find_by methods to dynamically create

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -16,44 +16,16 @@ class CustomerRepository
     all.sample
   end
 
-  def find_by_id(criteria)
-    all.find{|customer| customer.id == criteria}
+  %w(id first_name last_name created_at updated_at).each do |attribute|
+    define_method("find_by_#{attribute}") do |criteria| 
+      all.find{|c| c.send(attribute) == criteria}
+    end
   end
-
-  def find_all_by_id(criteria)
-    all.find_all{|customer| customer.id == criteria}
-  end
-
-  def find_by_first_name(criteria)
-    all.find{|customer| customer.first_name == criteria}
-  end
-
-  def find_all_by_first_name(criteria)
-    all.find_all{|customer| customer.first_name == criteria}
-  end
-
-  def find_by_last_name(criteria)
-    all.find{|customer| customer.last_name == criteria}
-  end
-
-  def find_all_by_last_name(criteria)
-    all.find_all{|customer| customer.last_name == criteria}
-  end
-
-  def find_by_created_at(criteria)
-    all.find{|customer| customer.created_at == criteria}
-  end
-
-  def find_all_by_created_at(criteria)
-    all.find_all{|customer| customer.created_at == criteria}
-  end
-  
-  def find_by_updated_at(criteria)
-    all.find{|customer| customer.updated_at == criteria}
-  end
-
-  def find_all_by_updated_at(criteria)
-    all.find_all{|customer| customer.updated_at == criteria}
+ 
+ %w(id first_name last_name created_at updated_at).each do |attribute|
+    define_method("find_all_by_#{attribute}") do |criteria| 
+      all.find_all{|c| c.send(attribute) == criteria}
+    end
   end
 
   def create_customers
