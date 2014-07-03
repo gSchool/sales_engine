423d8e2cf0dc01a236ecb8d020c27008fdc4106f
Hilary <hilarydenton90@gmail.com>
2014-07-03 10:45:34 -0600

    Update transaction_repository.rb

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -12,9 +12,7 @@ class TransactionRepository
     @sales_engine = engine
   end
 
-  def find_invoices(id)
-    transactions = sales_engine.find_transactions_by(id, "invoice_id")
-    invoice_id = transactions.map {|transaction| transaction.invoice_id}.first
+  def find_invoices(invoice_id)
     sales_engine.find_invoices_by(invoice_id, "id")
   end
 end
